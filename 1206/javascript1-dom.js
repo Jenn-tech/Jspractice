0e503966693e73e0dc56627bb2d4d214f092c39f
unknown <wogus789789@naver.com>
2020-12-12 23:23:58 +0900

    appendChilde, remove

--- 1206/javascript1-dom.js
+++ 1206/javascript1-dom.js
@@ -1,17 +1,23 @@
 window.addEventListener("load", function(){
-   var section = document.querySelector("#section6");
-   var titleInput = section.querySelector(".title-input");
-   var menuListDiv = section.querySelector(".menu-list");
-   var addButton = section.querySelector(".add-button");
-   var delButton = section.querySelector(".del-button");
+    var section = document.querySelector("#section6");
+    var titleInput =section.querySelector(".title-input"); 
+    var menuListUl =section.querySelector(".menu-list"); 
+    var addButton = section.querySelector(".add-button");
+    var delButton = section.querySelector(".del-button");
     
     addButton.onclick = function(){
         var title = titleInput.Value;
-        var txtNode = document.createTextNode(title);
-        menuListDiv.appendChild(txtNode);
+        var html = '<a href="">'+title+'</a>';
+        var li = document.createElement("li");
+        li.innerHTML = html;
+
+        menuListUl.appendChild(li);
     };
     delButton.onclick = function(){
-        var txtNode = menuListDiv.childNodes[0];
-        menuListDiv.removeChild(txtNode); 
+      //  var txtNode = menuListDiv.childNodes[0];
+      //  menuListDiv.removeChild(txtNode); 
+
+        var liNode = menuLisUI.children[0];
+        liNode.remove();
     };
 });
